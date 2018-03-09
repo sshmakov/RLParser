4e6f6c3a8015abf6c6c39042806400d8e6bb134c
sshmakov <sergey-shmakov@yandex.ru>
2018-03-09 14:01:03 +0300

    добавлен парсинг текста

--- phrases.py
+++ phrases.py
@@ -1,10 +1,16 @@
-
+# http://pymorphy2.readthedocs.io/en/latest/user/grammemes.html#grammeme-docs
 source = '''
 Вася ест кашу
 # сущ  гл  сущ
 # что/кто  делает с_чем-то
 NOUN,nomn VERB NOUN,accs
 
+Красивый цветок
+ADJF NOUN
+
+Птица сидит на крыше
+# сущ  гл  предлог сущ
+NOUN,nomn VERB NOUN,loct
 '''
 
 text = '''
@@ -13,12 +19,47 @@ text = '''
 Лара сама мыла раму
 Рано ушла наша Шура
 Мама мыла пластиковые окна
+
+Наша семья
+У нас большая семья
+Папа и брат Илья работают на заводе
+Мама ведет хозяйство
+Сестра Татьяна - учительница
+Я учусь в школе
+Младшие братья Миша и Вова ходят в детский сад
 '''
+
+import pymorphy2 as py
 
 class PPattern:
     def __init__(self):
         super().__init__()
 
+    def checkPhrase(self,text):
+        def checkWordTags(tags, grams):
+            for t in tags:
+                if t not in grams:
+                    return False
+            return True
+        def checkWord(tags, word):
+            variants = morph.parse(word)
+            for v in variants:
+                if checkWordTags(self.tags[nextTag].split(','), v.tag.grammemes):
+                    return (word, v)
+            return None
+        
+        morph = py.MorphAnalyzer()
+        words = text.split()
+        nextTag = 0
+        result = []
+        for w in words:
+            res = checkWord(self.tags[nextTag].split(','), w)
+            if res is not None:
+                result.append(res)
+                nextTag = nextTag + 1
+                if nextTag >= len(self.tags):
+                    return result
+        return None
 
 import io
 
@@ -48,5 +89,25 @@ def parseSource(src):
     return arr
 
 
+def parseText(pats, text):
+    def parseLine(line):
+        was = False
+        for p in pats:
+            res = p.checkPhrase(line)
+            if res:
+                print('+',line, p.tags, [r[0] for r in res])
+                was = True
+        if not was:                
+            print('-',line)
+
+    buf = io.StringIO(text)
+    s = buf.readline()
+    while s:
+        s = s.strip()
+        if s != '':
+            parseLine(s)
+        s = buf.readline()
+
 #print(a.split('\n'))
-s = parseSource(source)
+patterns = parseSource(source)
+parseText(patterns, text)
